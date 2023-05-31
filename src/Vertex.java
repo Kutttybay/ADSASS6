d6698b3c1e5e9272c33a9d3e65453a4f01ae1ffc
kutttybay <kutttybay@gmail.com>
2023-05-31 21:06:50 +0600

    5 commit

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,3 +1,26 @@
-public class Vertex {
+import java.util.*;
+public class Vertex<V> {
+    private V data;
+    private Map<Vertex<V>, Double> adjVertices;
+    public void addAdjVertex(Vertex<V> dest, double weight){
+        adjVertices.put(dest, weight);
+    }
 
+    public Map<Vertex<V>, Double> getAdjVertices(){
+        return adjVertices;
+    }
+
+    public V getData(){
+        return data;
+    }
+
+    public Vertex(V data){
+        this.data = data;
+        this.adjVertices = new HashMap<>();
+    }
+
+    @Override
+    public String toString(){
+        return "Vertex " + this.data;
+    }
 }
