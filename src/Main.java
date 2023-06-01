492992422c3c18721e86fcdc6ecba2c416a7d61b
kutttybay <kutttybay@gmail.com>
2023-06-01 07:36:17 +0600

    8 commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Assignment 6");
-
+        MyGraph<Integer> myGraph = new MyGraph<>();
+        Vertex<Integer> x1 = new Vertex<>(1);
+        Vertex<Integer> x2 = new Vertex<>(2);
+        Vertex<Integer> x3 = new Vertex<>(3);
+        Vertex<Integer> x4 = new Vertex<>(4);
+        Vertex<Integer> x5 = new Vertex<>(5);
+        myGraph.addEdge(x1, x2, 5d);
+        myGraph.addEdge(x1, x3, 6d);
+        myGraph.addEdge(x2, x4, 7d);
+        myGraph.addEdge(x2, x5, 8d);
 
     }
 }
